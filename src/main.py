d0192cc3568e41e785954efe0c7caa9ec805569d
EFarmer321 <eliasfarmer321@gmail.com>
2025-05-08 15:55:59 -0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -4,19 +4,22 @@ from app import app
 from utils import *
 from database import init_database
 from rate_limiter import create_end_point
+import getpass
+
 
 async def test_function():
     return JSONResponse(content={"response": "HELLO YES HELLO!!!!!@!@#!!@#!!!"}, status_code=200)
 
+
 def anti_ddos_init():
     port = 0
 
-    create_end_point("/test", ["GET"], 10, 50, test_function, 15)
-
+    create_end_point("/test", ["GET"], 5, 10, test_function, 15)
 
     while True:
-        port = input(f"Enter your port number ({PORT_RANGE_MIN} - {PORT_RANGE_MAX}): ")
-        
+        port = input(
+            f"Enter your port number ({PORT_RANGE_MIN} - {PORT_RANGE_MAX}): ")
+
         try:
             port = int(port)
         except:
@@ -30,10 +33,11 @@ def anti_ddos_init():
 
     print("Input your MySQL password and username. If the database has not been created yet, one will be made for you.")
     username = input("Enter your database username: ")
-    password = input("Enter database password: ")
+    password = getpass.getpass("Enter database password: ")
     init_database(username, password)
 
     uvicorn.run(app, host="127.0.0.1", port=port)
+
 
 if __name__ == "__main__":
     anti_ddos_init()
